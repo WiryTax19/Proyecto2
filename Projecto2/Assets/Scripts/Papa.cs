0ebe029ac7787a826fb5e4d7ac1ae74feddeecb4
AAIE02 <77703459+WiryTax19@users.noreply.github.com>
2022-09-13 08:56:54 -0500

    TimerCreado
    Aprendi a hacer corrutinas y con ellas, cree el timer para que la bola al caer regrese al jugador.

--- Projecto2/Assets/Scripts/Papa.cs
+++ Projecto2/Assets/Scripts/Papa.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using UnityEngine;
 
 public class Papa : MonoBehaviour
 {
     public GameObject bolita;
     public GameObject Player;
+
     //public GameObject explosion;
     //float time;
 
     // Start is called before the first frame update
     void Start()
     {
+        bolita.transform.SetParent(Player.transform);
+        bolita.GetComponent<Rigidbody>().useGravity = false;
+        bolita.transform.localPosition = new Vector3(0, 2, 0);
         //time += Time.deltaTime;
         //if (time >= 3) Instantiate(explosion, transform.position, Quaternion.identity);
         //{
-            //Destroy(gameObject, 3);
+        //Destroy(gameObject, 3);
         //}
     }
 
@@ -24,8 +30,8 @@ public class Papa : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.F))
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.forward * 500);
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 500);
+            bolita.GetComponent<Rigidbody>().AddForce(Vector3.forward * 500);
+            bolita.GetComponent<Rigidbody>().AddForce(Vector3.up * 500);
             bolita.transform.SetParent(null);
             bolita.GetComponent<Rigidbody>().useGravity = true;
             bolita.GetComponent<Collider>().isTrigger = false;
@@ -36,24 +42,28 @@ public class Papa : MonoBehaviour
     {
         if (other.gameObject.tag == "ground")
         {
-
-            Debug.Log("Toco Piso");
-            bolita.transform.SetParent(Player.transform);
-            bolita.GetComponent<Rigidbody>().useGravity = false;
-            bolita.transform.localPosition = new Vector3(0, 2, 0);
-            
-        }    
+            StartCoroutine(TimerReturn());
+        }
 
         if (other.gameObject.tag == "player")
         {
-            Debug.Log("Toco Player");
             bolita.GetComponent<Collider>().isTrigger = true;
-            //Destroy(gameObject, 5f);
         }
 
         if (other.gameObject.tag == "playerother")
         {
             Debug.Log("Toco Playerother");
+            //Destroy(gameObject, 5f);
         }
     }
+
+    private IEnumerator TimerReturn()
+    {
+        yield return new WaitForSeconds(3);
+
+        bolita.transform.SetParent(Player.transform);
+        bolita.GetComponent<Rigidbody>().useGravity = false;
+        bolita.transform.localPosition = new Vector3(0, 2, 0);
+    }
+
 }
