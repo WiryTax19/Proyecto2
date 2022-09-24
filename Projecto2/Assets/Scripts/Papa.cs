d4c7bdd8d0ff74edca78e45e991d9386765a2d79
Santiago Tello Oseguera <santypadawan@gmail.com>
2022-09-24 12:59:07 -0500

    TocaExplotaRespawnea
    Hice una clase de Loop en la que la bola al tocar el piso espera segundos y regresa al jugador, espera segundos y explota, despues espera otros segundos para aparecer y reiniciar el proceso

--- Projecto2/Assets/Scripts/Papa.cs
+++ Projecto2/Assets/Scripts/Papa.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -9,19 +10,22 @@ public class Papa : MonoBehaviour
 {
     public GameObject bolita;
     public GameObject Player;
-
+    public bool itsHided = false;
+    public bool itsHolded = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        bolita.transform.SetParent(Player.transform);
-        bolita.GetComponent<Rigidbody>().useGravity = false;
+        //Bola se hace hija de player
+        bolita.transform.SetParent(null);
+        bolita.GetComponent<Rigidbody>().useGravity = true;
         bolita.transform.localPosition = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Se lanza la bola y se quita de padre player
         if(Input.GetKeyDown(KeyCode.F))
         {
             bolita.GetComponent<Rigidbody>().AddForce(transform.forward * 250);
@@ -31,43 +35,48 @@ public class Papa : MonoBehaviour
             bolita.GetComponent<Collider>().isTrigger = false;
         }
 
-        if(bolita.activeSelf == false)
+        if(itsHided == true)
         {
-            
+            Debug.Log("Se Escondio");
+            StartCoroutine(TimerRespawn());
         }
+
+        //if (bolita.transform.parent.parent == Player) 
+        //{
+            //Debug.Log("itsHolded True");
+        //}
+
+        //if(itsHolded == true)
+        //{
+            //StartCoroutine(TimerExplossion());
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //Si toca el piso empieza el timer para regresar
         if (other.gameObject.tag == "ground")
         {
             StartCoroutine(TimerReturn());
         }
 
+        //Si toca al player empieza el timer de explosion
         if (other.gameObject.tag == "player")
         {
-            //while (true)
-            {
-                StartCoroutine(TimerExplossion());
-            }
+            itsHolded = true;
+            StartCoroutine(TimerExplossion());
         }
 
         if (other.gameObject.tag == "playerother")
         {
-            Debug.Log("Toco Playerother");
-            //Destroy(gameObject, 5f);
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.gameObject.tag == "ground")
-        {
-            //StartCoroutine(TimerExplossion());
+            
         }
     }
 
     private IEnumerator TimerReturn()
     {
+        //Regresa con Player despues de los segundos
+        Debug.Log("Empezo TimerReturn");
         yield return new WaitForSeconds(3);
 
         bolita.transform.SetParent(Player.transform);
@@ -77,10 +86,25 @@ public class Papa : MonoBehaviour
 
     private IEnumerator TimerExplossion()
     {
-        yield return new WaitForSeconds(5);
-        Debug.Log("Esto");
-        bolita.SetActive(false);
+        //Se esconde si pasan los segundos
+        Debug.Log("Empezo TimerExplossion");
+        yield return new WaitForSeconds(8);
+        Debug.Log("BUUUM Explotaste");
+        bolita.GetComponent<Renderer>().enabled = false;
         bolita.transform.SetParent(null);
-        yield return new Null();
+        bolita.transform.localPosition = new Vector3(0, 2, 0);
+        itsHided = true;
+    }
+
+    private IEnumerator TimerRespawn()
+    {
+        Debug.Log("Empezo TimerRespawn");
+        itsHided = false;
+        yield return new WaitForSeconds(3);
+
+        bolita.GetComponent<Rigidbody>().useGravity = true;
+        bolita.GetComponent<Renderer>().enabled = true;
+        bolita.transform.localPosition = new Vector3(1, 2, 0);
+
     }
 }
